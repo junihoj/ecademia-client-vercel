5969ee7317f3200111bd3c560bf0bc100163b47e
junihoj <ezeonyekachukwu98@gmail.com>
2023-08-19 17:35:15 +0100

    added temporary logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+         <link rel="icon" href="/api-interface-svgrepo-com.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
