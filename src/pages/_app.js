da950a3d5a3c48d893a22e7f8cab5b35fab4c484
junihoj <ezeonyekachukwu98@gmail.com>
2023-09-03 14:32:55 +0100

    initial project commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import theme from '@/config/theme-config';
+
+const App = ({ Component, pageProps }) => (
+  <ConfigProvider theme={theme}>
+    <Component {...pageProps} />
+  </ConfigProvider>
+);
+
+export default App;
